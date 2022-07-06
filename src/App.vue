16333f055ebdddbc65f5948a1323effc152cc680
Neo-Machina <andreistatache1997@gmail.com>
2022-07-06 18:33:44 +0200

    header almost completed

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,26 @@
 <template>
   <div id="app">
+    <PageHeader />
+
+    <main>
+
+    </main>
+
+    <Footer />
   </div>
 </template>
 
 <script>
+import PageHeader from "./components/PageHeader.vue";
 
 export default {
   name: 'App',
   components: {
+    PageHeader
   }
 }
 </script>
 
 <style lang="scss">
-@import './style/common.scss'
+@import './style/common'
 </style>
---
+++ src/components/PageHeader.vue
@@ -0,0 +1,126 @@
+<template>
+    <header>
+        <div class="container flex-header">
+            <!-- header-logo -->
+            <div class="logo">
+                <img src="../assets/img/dc-logo.png" alt="logo-dc">
+            </div>
+
+            <!-- header-nav -->
+            <nav>
+                <ul>
+                    <li v-for="(link, index) in menuLinks" :key="index" :class="{ 'selected-item' : link.current }">
+                        <a :href="url">
+                            {{ link.text }}
+                        </a>
+                    </li>          
+                </ul>
+            </nav>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'PageHeader',
+    data() {
+        return {
+            menuLinks: [
+                {
+                    text: 'characters',
+                    url: '#',
+                    current: false
+                },
+                {
+                    text: 'comics',
+                    url: '#',
+                    current: true
+                },
+                {
+                    text: 'movies',
+                    url: '#',
+                    current: false
+                },
+                {
+                    text: 'tv',
+                    url: '#',
+                    current: false
+                },
+                {
+                    text: 'games',
+                    url: '#',
+                    current: false
+                },
+                {
+                    text: 'collectibles',
+                    url: '#',
+                    current: false
+                },
+                {
+                    text: 'videos',
+                    url: '#',
+                    current: false
+                },
+                {
+                    text: 'fans',
+                    url: '#',
+                    current: false
+                },
+                {
+                    text: 'news',
+                    url: '#',
+                    current: false
+                },
+                {
+                    text: 'shop',
+                    url: '#',
+                    current: false
+                }
+        ]   
+        }
+    } 
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../style/variables';
+
+header {
+    background-color: pink;
+    height: 90px;
+
+    .flex-header {
+    display: flex;
+    justify-content: space-between;
+    height: 100%;
+    align-items: center;
+    }
+
+    .logo {
+        img {
+            width: 60px;
+        }
+    }
+
+    nav {
+        ul {
+            display: flex;
+
+            li {
+                padding: 0 10px;
+
+                &.selected-item {
+                    display: inline-block;
+                    line-height: 0.9;
+                    border-bottom: 4px solid $brand_main_color;      
+                }
+
+                a {
+                    font-size: 10px;
+                    text-transform: uppercase;
+                }
+            }
+        }
+    }
+}
+</style>
